9294f061a668c16786d42b39436df9ec1cefd3c5
david <dseibert@friedmanvartolo.com>
2025-06-02 14:44:03 -0400

    resimplified the mindserver

--- mindserver/src/entrypoint.js
+++ mindserver/src/entrypoint.js
@@ -1,4 +1,4 @@
-import { createMindServer } from './mind_server.js';
+import { createMindServer } from './server.js';
 
 const port = process.env.MINDSERVER_PORT || 8080;
 const server = createMindServer(port);
