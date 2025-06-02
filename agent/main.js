0018438995e03048efe018c25a67bb855e00c6c7
david <dseibert@friedmanvartolo.com>
2025-06-01 21:35:41 -0400

    removed argument handling

--- agent/main.js
+++ agent/main.js
@@ -1,37 +1,7 @@
-// import { AgentProcess } from './src/process/agent_process.js';
 import settings from './settings.js';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-// import { mainProxy } from './src/process/main_proxy.js';
-import { readFileSync } from 'fs';
 import {Agent} from "./src/agent/agent.js";
 
-function parseArguments() {
-    // eslint-disable-next-line no-undef
-    return yargs(hideBin(process.argv))
-        .option('profiles', {
-            type: 'array',
-            describe: 'List of agent profile paths',
-        })
-        .option('task_path', {
-            type: 'string',
-            describe: 'Path to task file to execute'
-        })
-        .option('task_id', {
-            type: 'string',
-            describe: 'Task ID to execute'
-        })
-        .help()
-        .alias('help', 'h')
-        .parse();
-}
-
-function getProfiles(args) {
-    return args.profiles || settings.profiles;
-}
-
 async function main() {
-    // const args = parseArguments();
     const { load_memory, init_message , profiles} = settings;
     const profile = profiles[0];
 
